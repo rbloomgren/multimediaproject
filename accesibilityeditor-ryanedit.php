e218129873b6a90d0686ae9c85ca64e095ce4466
rbloomgren <rbloomgren@gmail.com>
2018-08-19 17:04:40 -0400

    ryan ocr code

---
+++ accesibilityeditor-ryanedit.php
@@ -0,0 +1,19 @@
+<html>
+    <script type="text/javascript">
+    function init() {
+        
+    }
+    
+    </script>
+	
+	
+	
+    <body onload="init()">
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+	
+    </body>
+    </html>
